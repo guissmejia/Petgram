a3ff441d7694c29dc4e337feb758789459298e6c
Guiselle Mejia <guissmejia@gmail.com>
2020-09-29 16:58:56 -0500

    Deployment in Vercel

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render('Holi Guiselle!', document.getElementById('app'));
+ReactDOM.render('Holi Guiselle!', document.getElementById('app'))
