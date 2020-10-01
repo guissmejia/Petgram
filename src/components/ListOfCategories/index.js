34a8b51d533a3045bf932c79077ac061f90aac1c
Guiselle Mejia <guissmejia@gmail.com>
2020-09-30 19:52:37 -0500

    Using useState and useEffect for events

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,14 +1,48 @@
-import React from 'react'
-import { Category } from '../Category'
-import { List, Item } from './styles'
-import { categories } from '../../../api/db.json'
+import React, { Fragment, useEffect, useState } from 'react';
+import { Category } from '../Category';
+
+import { List, Item } from './styles';
 
 export const ListOfCategories = () => {
-  return (
-    <List>
-      {
-        categories.map(category => <Item key={category.id}> <Category {...category}/> </Item>)
-      }
+  const [categories, setCategories] = useState([]);
+  const [showFixed, setShowFixed] = useState(false);
+
+  useEffect(function () {
+    window
+      .fetch('https://petgram-api.guissmejia.vercel.app/categories')
+      .then((res) => res.json())
+      .then((response) => {
+        setCategories(response);
+      });
+  }, []);
+
+  useEffect(
+    function () {
+      const onScroll = (e) => {
+        const newShowFixed = window.scrollY > 200;
+        showFixed !== newShowFixed && setShowFixed(newShowFixed);
+      };
+      document.addEventListener('scroll', onScroll);
+
+      return () => document.removeEventListener('scroll', onScroll);
+    },
+    [showFixed]
+  );
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
+      {categories.map((category) => (
+        <Item key={category.id}>
+          <Category {...category} />
+        </Item>
+      ))}
     </List>
-  )
-}
+  );
+
+  return (
+    <Fragment>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </Fragment>
+  );
+};
--- src/components/PhotoCard/styles.js
+++ src/components/PhotoCard/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { fadeIn } from '../../styles/animation'
 
 export const ImgWrapper = styled.div`
   border-radius: 10px;
@@ -7,10 +8,11 @@ export const ImgWrapper = styled.div`
   overflow: hidden;
   padding: 56.25% 0 0 0;
   position: relative;
-  width: 100%;
+  width: 100%; 
 `
 
 export const Img = styled.img`
+  ${fadeIn()}
   box-shadow: 0 10px 14px rgba(0, 0, 0, .2);
   height: 100%;
   object-fit: cover;
@@ -20,11 +22,9 @@ export const Img = styled.img`
 `
 
 export const Button = styled.button`
-  padding-top: 8px;
-  background: transparent;
-  border: none;
   display: flex;
   align-items: center;
+  padding-top: 8px;
   & svg {
     margin-right: 4px;
   }
