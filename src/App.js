fc048e05b873363dbb9f86a88ebc9b32f8949dcf
Guiselle Mejia <guissmejia@gmail.com>
2020-10-02 18:42:50 -0500

    Protecting the routes through parameters that identify if the user is logged in or not

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 import React from 'react';
-import { Logo } from './components/Logo';
-import { Home } from './pages/Home';
-import { Router } from '@reach/router';
 import { GlobalStyle } from './styles/GlobalStyles';
+import { Logo } from './components/Logo';
+import { NavBar } from './components/NavBar';
+
+import { Home } from './pages/Home';
 import { Detail } from './pages/Detail';
+import { Favs } from './pages/Favs';
+import { User } from './pages/User';
+import { NotRegisteredUser } from './pages/NotRegisteredUser';
+
+import { Router } from '@reach/router';
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: true });
+};
 
 export const App = () => {
-  const urlParams = new window.URLSearchParams(window.location.search);
-  const detailId = urlParams.get('detail');
   return (
     <div>
       <GlobalStyle />
       <Logo />
       <Router>
         <Home path="/" />
-        <Home path="/pet/:id" />
+        <Home path="/pet/:categoryId" />
         <Detail path="/detail/:detailId" />
       </Router>
+
+      <UserLogged>
+        {({ isAuth }) =>
+          isAuth ? (
+            <Router>
+              <Favs path="/favs" />
+              <User path="/user" />
+            </Router>
+          ) : (
+            <Router>
+              <NotRegisteredUser path="/favs" />
+              <NotRegisteredUser path="/user" />
+            </Router>
+          )
+        }
+      </UserLogged>
+
+      <NavBar />
     </div>
   );
 };
