8314c0f9412631bfd50a68a9d3f8d1654c2561aa
Guiselle Mejia <guissmejia@gmail.com>
2020-10-08 16:56:55 -0500

    Handlers added for error and loading status when registering a user

--- src/App.js
+++ src/App.js
@@ -8,12 +8,8 @@ import { Detail } from './pages/Detail';
 import { Favs } from './pages/Favs';
 import { User } from './pages/User';
 import { NotRegisteredUser } from './pages/NotRegisteredUser';
-
+import Context from './Context'
 import { Router } from '@reach/router';
-
-const UserLogged = ({ children }) => {
-  return children({ isAuth: true });
-};
 
 export const App = () => {
   return (
@@ -26,7 +22,7 @@ export const App = () => {
         <Detail path="/detail/:detailId" />
       </Router>
 
-      <UserLogged>
+      <Context.Consumer>
         {({ isAuth }) =>
           isAuth ? (
             <Router>
@@ -40,7 +36,7 @@ export const App = () => {
             </Router>
           )
         }
-      </UserLogged>
+      </Context.Consumer>
 
       <NavBar />
     </div>
