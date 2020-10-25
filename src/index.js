43e2adffd37488e8a3d50b3115d6fe88d747bcda
Guiselle Mejia <guissmejia@gmail.com>
2020-10-25 12:19:38 -0500

    The mutation for the login is created and the user is allowed to like as a registered user only

--- src/index.js
+++ src/index.js
@@ -7,6 +7,22 @@ import Context  from './Context';
 
 const client = new ApolloClient({
   uri: 'https://petgram-api.guissmejia.vercel.app/graphql',
+  request: operation => {
+    const token = window.sessionStorage.getItem('token')
+    const authorization = token ? `Bearer ${token}` : ''
+    operation.setContext({
+      headers: {
+        authorization
+      }
+    })
+  },
+  onError: error => {
+    const { networkError } = error
+    if(networkError && networkError.result.code === 'invalid_token') {
+      window.sessionStorage.removeItem('token')
+      window.location.href = '/'
+    }
+  }
 });
 
 ReactDOM.render(
--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,29 +1,61 @@
-import React from 'react';
-import Context from '../Context';
+import React, { useContext } from 'react';
+import { Context } from '../Context';
 import { UserForm } from '../components/UserForm';
 import { RegisterMutation } from '../containers/RegisterMutation';
+import { LoginMutation } from '../containers/LoginMutation';
 
-export const NotRegisteredUser = () => (
-  <Context.Consumer>
-    {({ activateAuth }) => {
-      return (
-        <>
-          <RegisterMutation>
-            {(register, { data, loading, error }) => {
-              const onSubmit = ({ email, password }) => {
-                const input = { email, password }
-                const variables = { input }
-                register({ variables }).then(activateAuth)
-              }
+export const NotRegisteredUser = () => {
+  const { activateAuth } = useContext(Context);
+  return (
+    <>
+      <RegisterMutation>
+        {(register, { data, loading, error }) => {
+          const onSubmit = ({ email, password }) => {
+            const input = { email, password };
+            const variables = { input };
+            register({ variables }).then(({ data }) => {
+              const { signup } = data;
+              //console.log(response)
+              activateAuth(signup);
+            });
+          };
 
-              const errorMsg = error && 'El usuario ya existe'
+          const errorMsg = error && 'El usuario ya existe';
 
-              return <UserForm  disabled={loading} error={errorMsg} title="Registrarse" onSubmit={onSubmit} />;
-            }}
-          </RegisterMutation>
-          <UserForm title="Iniciar Sesion" onSubmit={activateAuth} />
-        </>
-      );
-    }}
-  </Context.Consumer>
-);
+          return (
+            <UserForm
+              disabled={loading}
+              error={errorMsg}
+              title="Registrarse"
+              onSubmit={onSubmit}
+            />
+          );
+        }}
+      </RegisterMutation>
+      <LoginMutation>
+        {(login, { data, loading, error }) => {
+          const onSubmit = ({ email, password }) => {
+            const input = { email, password };
+            const variables = { input };
+            login({ variables }).then(({ data }) => {
+              const { login } = data;
+              //console.log(response)
+              activateAuth(login);
+            });
+          };
+
+          const errorMsg = error && 'Inicio de Sesion no valido';
+
+          return (
+            <UserForm
+              disabled={loading}
+              error={errorMsg}
+              title="Iniciar Sesion"
+              onSubmit={onSubmit}
+            />
+          );
+        }}
+      </LoginMutation>
+    </>
+  );
+};
