56813f2eb5f656969c12bb7daba60affd975a4a4
Guiselle Mejia <guissmejia@gmail.com>
2020-10-26 20:29:11 -0500

    PWA: Generating the Manifest

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,29 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { pathToArray } = require('graphql/jsutils/Path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifestPlugin = require('webpack-pwa-manifest');
+const path = require('path');
 
 module.exports = {
   output: {
-    filename: "app.bundle.js",
-    publicPath: '/'
+    filename: 'app.bundle.js',
+    publicPath: '/',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "src/index.html",
+      template: 'src/index.html',
+    }),
+    new WebpackPwaManifestPlugin({
+      name: 'Petgram - Tu App de fotos de mascotas',
+      shortname: 'Petgram',
+      description: 'Aqui encontraras fotos de animales hermosos',
+      background_color: '#fff',
+      theme_color: '#b1a',
+      icons: [
+        {
+          src: path.resolve('src/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+        },
+      ],
     }),
   ],
   module: {
@@ -16,9 +32,9 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
+            presets: ['@babel/preset-env', '@babel/preset-react'],
           },
         },
       },
